2f64c5174ce0da3fff4aaaecf765ac81da0d7958
surya <surya@SuryaLinuxMintLenovo>
2017-02-04 23:07:49 +0530

    minmax done

--- HackerRank/src/algorithms/MinMaxProblem.java
+++ HackerRank/src/algorithms/MinMaxProblem.java
@@ -4,76 +4,62 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class MinMaxProblem {
-	
-	
-	
-	
+
+	// 5 4 3 2 1
+
 	static long[] array;
-	
-	
-	
+
 	public static void main(String[] args) {
 
+		Scanner in = new Scanner(System.in);
+		long a = in.nextLong();
+		long b = in.nextLong();
+		long c = in.nextLong();
+		long d = in.nextLong();
+		long e = in.nextLong();
 
-		
-		 Scanner in = new Scanner(System.in);
-	        long a = in.nextLong();
-	        long b = in.nextLong();
-	        long c = in.nextLong();
-	        long d = in.nextLong();
-	        long e = in.nextLong();
-	        
-	        long minSum=0;
-	        long maxSum=0;
-	        
-	         array= new long[5];
-	        
-	        array[0]=a;
-	        array[1]=b;
-	        array[2]=c;
-	        array[3]=d;
-	        array[4]=e;
-	        
-	        sort();
-	        printArray();
-		
+		long minSum = 0;
+		long maxSum = 0;
+
+		array = new long[5];
+
+		array[0] = a;
+		array[1] = b;
+		array[2] = c;
+		array[3] = d;
+		array[4] = e;
+
+		sort();
+		// printArray();
+		System.out
+				.print((array[0] + array[1] + array[2] + array[3]) + " " + (array[1] + array[2] + array[3] + array[4]));
+
 	}
 
-	public static void sort()
-	{
-		long temp=0;
-		for(int i=0;i<array.length;i++)
-		{System.out.println("he");
-			for(int j=i+1;j<array.length;j++)
-			{
+	public static void sort() {
+		long temp = 0;
+		for (int i = 0; i < array.length; i++) {
 
-				if(array[i]>array[j])
-				{
-					
-					temp=array[i];
-					array[i]=array[j];
-					array[j]=temp;
-					break;
+			for (int j = i + 1; j < array.length; j++) {
+
+				if (array[i] > array[j]) {
+
+					temp = array[i];
+					array[i] = array[j];
+					array[j] = temp;
+
 				}
 			}
 		}
-		
-		
-		
+
 	}
-	
-	
-	public static void printArray()
-	{
-		
-		for(long i:array)
-		{System.out.print(i+" ");
+
+	public static void printArray() {
+
+		for (long i : array) {
+			System.out.print(i + " ");
 
 		}
 	}
-	
-	
-	
-	
-	
+
 }
