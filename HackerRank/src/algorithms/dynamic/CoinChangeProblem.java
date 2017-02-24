1ded193f88c508cecc16869a8de8c047bfc7fbfe
surya <surya@SuryaLinuxMintLenovo.com>
2017-02-24 09:33:25 +0530

    Created a stack overflow

--- HackerRank/src/algorithms/dynamic/CoinChangeProblem.java
+++ HackerRank/src/algorithms/dynamic/CoinChangeProblem.java
@@ -2,6 +2,32 @@ package algorithms.dynamic;
 
 public class CoinChangeProblem {
 
-	int[][][] mem;
-
+	static int a[]={1,2,10};
+	
+	public static void main(String[] args) {
+		getCoinChange(0,10 , "");
+	}
+	
+	public static void getCoinChange(int start, int expSum, String op){
+	
+		
+		
+		
+		for(int i=1;i<=expSum/i;i++){
+			System.out.println(i+" "+a[start]);
+			if(i*a[start]>expSum)
+				return;
+			if(i*a[start]==expSum){
+				op=op+"+ "+a[start]+"x"+i;
+				System.out.println(op);
+				return;
+			}else
+				op=op+"+ "+a[start]+"x"+i;
+				getCoinChange(start, expSum, op);
+		}
+		
+	}
+	
+	
+	
 }
