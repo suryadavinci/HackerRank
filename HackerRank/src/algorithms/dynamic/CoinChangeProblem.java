6805a0da264e9b869638b6f86fa9d425a678439f
surya <surya@SuryaLinuxMintLenovo.com>
2017-02-26 23:05:43 +0530

    Coin Change with no memoisation

--- HackerRank/src/algorithms/dynamic/CoinChangeProblem.java
+++ HackerRank/src/algorithms/dynamic/CoinChangeProblem.java
@@ -1,30 +1,75 @@
 package algorithms.dynamic;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
+
+
 public class CoinChangeProblem {
 
-	static int a[]={1,2,10};
-	
+	//static int a[]={10,2,1};
+	static int a[];
+	static int count;
 	public static void main(String[] args) {
-		getCoinChange(0,10 , "");
+		
+		/*HashMap<Integer,Integer> h= new HashMap<Integer,Integer>();
+		h.put(1, 1);
+		h.put(2, 5);
+		
+		HashMap<Integer,Integer> i= new HashMap<Integer,Integer>();
+		i.put(3, 1);
+		i.put(4, 5);
+		i.putAll(h);
+		
+			System.out.println(i);
+		*/
+		
+		
+		Scanner s= new Scanner(System.in);
+		int n=s.nextInt();
+		/*4 3
+		1 2 3 */
+		int m=s.nextInt();
+		
+		a=new int[m];
+		for(int i=0;i<m;i++){
+			a[i]=s.nextInt();
+		}
+		
+		getCoinChange(n,a, new HashMap<Integer,Integer>());
+		
+		System.out.println(count);
 	}
 	
-	public static void getCoinChange(int start, int expSum, String op){
+	public static void getCoinChange(int sum, int[] sub, HashMap<Integer,Integer> x){
+			//System.out.println("INPUT ARRAY : "+Arrays.toString(sub));
+		
+			if(sub.length==0){
 	
+			return;
+			}
+			int a=sub[0];		
+			for(int j=0;j<=sum/sub[0];j++){
+				if(j*a==sum){
+					x.put(a, j);
+					count++;
+					//System.out.println("FOUND "+x);
+					x.remove(a);
+				}
+				else if(j*a>sum){
+					break;
+				}else{
+				//	op=op+""+a+"x"+j+" + ";
+					System.out.println(sum+" "+x);
+					x.put(a, j);
+
+					getCoinChange(sum-a*j, Arrays.copyOfRange(sub, 1,sub.length ),x);
+				}
+				
+			}
 		
+
 		
-		
-		for(int i=1;i<=expSum/i;i++){
-			System.out.println(i+" "+a[start]);
-			if(i*a[start]>expSum)
-				return;
-			if(i*a[start]==expSum){
-				op=op+"+ "+a[start]+"x"+i;
-				System.out.println(op);
-				return;
-			}else
-				op=op+"+ "+a[start]+"x"+i;
-				getCoinChange(start, expSum, op);
-		}
 		
 	}
 	
