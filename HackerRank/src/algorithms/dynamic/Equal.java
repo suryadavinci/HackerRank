869b97d4f48d123cb5466df33279b89f4901d822
gnanasuriyan.g <gnanasuriyan.g@D7240.prodapt.com>
2017-02-28 14:41:12 +0530

    Equal still pending

--- HackerRank/src/algorithms/dynamic/Equal.java
+++ HackerRank/src/algorithms/dynamic/Equal.java
@@ -9,96 +9,125 @@ import java.util.SortedMap;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
-
-
 public class Equal {
 
 	static int[] a;
 	static int sHigh;
 	static int tHigh;
+	static int currentCount;
+	static int n=0;
 	static int count;
-	
-	static TreeSet<Integer> set= new TreeSet<Integer>();
+
+
+	static TreeSet<Integer> set = new TreeSet<Integer>();
+
 	public static void main(String[] args) {
-        
-        Scanner s= new Scanner(System.in);
-        int t=s.nextInt();
-        int n;
-        for(int i=0;i<t;i++){
-            n=s.nextInt();
-            a= new int[n];
 
-            for(int j=0;j<n;j++){
-                
-                a[j]=s.nextInt();
-                
-                
-            }
-            count=0;
-            		getEqual();
+		//int[] a = { 1, 2, 11, 45, 13, 19, 66, 3, 4, 5 };
+		int[] a={2,2,3,7};
 
-            
-            
-        }
-        
+
+		System.out.println(getEqual(a, 0, 0));
 		
 	}
-	
-	
-	public static void getEqual(int n){
-		
-	}
-	
-	public static void getEqual(){
-		
-		
-		
-		//System.out.println("??? "+Arrays.toString(a));
 
-		sHigh=getNthLargest(2);
-		//System.out.println(sHigh);
-		try{
-			tHigh=getNthLargest(3);
-		}catch(NoSuchElementException n){
-			count++;
-			System.out.println(count);
-			return;
-		}
-		//System.out.println("2nd highest is "+sHigh+" third highest is "+tHigh);
-		int sum=a[sHigh]-a[tHigh];
-		for(int i=0;i<a.length;i++){
-			if(i!=sHigh)
-				a[i]=a[i]+sum;
-		}
-		count++;
-		//System.out.println(Arrays.toString(a));
-		
-		getEqual();
-	}
 	
-	
-	public static int getNthLargest(int n){
-		
-		set.removeAll(set);
-		
-		for(int i=0; i<a.length;i++)
-		set.add(a[i]);
-		
-		Iterator<Integer> i=set.descendingIterator();
-		int j=1;
-		while( j<n){
-			i.next();
-			j++;
-		}
-		int x=i.next();
-		int k;
-		for(k=0;k<a.length;k++)
-			if(a[k]==x){
-				break;
+
+	public static void tempmain(String[] args) {
+
+		Scanner s = new Scanner(System.in);
+		int t = s.nextInt();
+		int n;
+		for (int i = 0; i < t; i++) {
+			n = s.nextInt();
+			a = new int[n];
+
+			for (int j = 0; j < n; j++) {
+
+				a[j] = s.nextInt();
+
 			}
-		return k;
+
+		}
+
 	}
-	
-	
-	
+
+	public static int getEqual(int[] array, int val, int secHighIndex) {
+		
+		
+		
+		
+		
+		if (allElementsEqual(array)) {
+			System.out.println("all elements equal");
+			return 1;
+		}
+
+		int[] newArray = Arrays.copyOf(array, array.length);
+
+		if(n++==10)
+			System.exit(0);
+
+		if (val != 0) {
+			System.out.println("BEFORE :"+Arrays.toString(newArray));
+			newArray = addNToArray(newArray, secHighIndex, val);
+			System.out.println("AFTER :"+Arrays.toString(newArray));
+		}
+
+		int secondHighIndex = getNthLargest(2, newArray);
+
+		count = 1 + Math.min(
+				Math.min(currentCount=getEqual(newArray, 1, secondHighIndex),
+						currentCount=getEqual(newArray, 2, secondHighIndex)),
+						currentCount=getEqual(newArray, 5, secondHighIndex));
+		
+		
+
+		return count;
+	}
+
+	private static boolean allElementsEqual(int[] a) {
+		System.out.println(Arrays.toString(a));
+		
+		for (int i = 1; i < a.length-1; i++) {
+			
+			//System.out.println(a[i]+" "+a[i-1]);
+			if (a[i] != a[i - 1])
+				return false;
+		}
+		System.out.println("returning true");
+		return true;
+	}
+
+	public static int[] addNToArray(int[] a, int ignoreIndex, int val) {
+
+		for (int i = 0; i < a.length; i++) {
+
+			if (i != ignoreIndex) {
+				a[i] = a[i] + val;
+			}
+		}
+
+		return a;
+	}
+
+	public static int getNthLargest(int n, int[] a) {
+
+		Arrays.sort(a);
+		//System.out.println(Arrays.toString(a));
+		int high = a.length - 1;
+		int i = 0;
+		int count = 1;
+		for (i = a.length - 2; i >= 0; i--) {
+			if (a[i] < a[high]) {
+				//System.out.println(count + " " + a[i] + " " + a[high]);
+				count++;
+				if (count == n)
+					return i;
+			}
+		}
+		return i;
+
+	}
+
 }
