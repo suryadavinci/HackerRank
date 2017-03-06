a84e57b77c722be79eae053559b7835fa71d0738
surya <surya@SuryaLinuxMintLenovo.com>
2017-03-06 21:08:23 +0530

    Coin Change Done

--- HackerRank/src/algorithms/dynamic/CoinChangeProblem.java
+++ HackerRank/src/algorithms/dynamic/CoinChangeProblem.java
@@ -1,78 +1,54 @@
 package algorithms.dynamic;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Scanner;
 
-
 public class CoinChangeProblem {
 
-	//static int a[]={10,2,1};
-	static int a[];
-	static int count;
+	static long[] a;
+	static HashMap<String, Long> mem;
+
 	public static void main(String[] args) {
-		
-		/*HashMap<Integer,Integer> h= new HashMap<Integer,Integer>();
-		h.put(1, 1);
-		h.put(2, 5);
-		
-		HashMap<Integer,Integer> i= new HashMap<Integer,Integer>();
-		i.put(3, 1);
-		i.put(4, 5);
-		i.putAll(h);
-		
-			System.out.println(i);
-		*/
-		
-		
-		Scanner s= new Scanner(System.in);
-		int n=s.nextInt();
-		/*4 3
-		1 2 3 */
-		int m=s.nextInt();
-		
-		a=new int[m];
-		for(int i=0;i<m;i++){
-			a[i]=s.nextInt();
+
+		Scanner s = new Scanner(System.in);
+		int sum = s.nextInt();
+		/*
+		 * 4 3 1 2 3
+		 */
+		int m = s.nextInt();
+		mem = new HashMap<String, Long>();
+		a = new long[m];
+		for (int i = 0; i < m; i++) {
+			a[i] = s.nextInt();
 		}
-		
-		getCoinChange(n,a, new HashMap<Integer,Integer>());
-		
-		System.out.println(count);
-	}
-	
-	public static void getCoinChange(int sum, int[] sub, HashMap<Integer,Integer> x){
-			//System.out.println("INPUT ARRAY : "+Arrays.toString(sub));
-		
-			if(sub.length==0){
-	
-			return;
-			}
-			int a=sub[0];		
-			for(int j=0;j<=sum/sub[0];j++){
-				if(j*a==sum){
-					x.put(a, j);
-					count++;
-					System.out.println("FOUND "+x);
-					x.remove(a);
-				}
-				else if(j*a>sum){
-					break;
-				}else{
-				//	op=op+""+a+"x"+j+" + ";
-					//System.out.println(sum+" "+x);
-					x.put(a, j);
 
-					getCoinChange(sum-a*j, Arrays.copyOfRange(sub, 1,sub.length ),x);
-				}
-				
-			}
-		
-
-		
-		
+		System.out.println(getCoinChange(sum, 0));
+		System.out.println(mem);
+		s.close();
 	}
-	
-	
-	
+
+	private static long getCoinChange(long sum, int index) {
+		// TODO Auto-generated method stub
+
+		if (mem.containsKey(index + "-" + sum))
+			return mem.get(index + "-" + sum);
+
+		long count = 0;
+
+		for (int i = 0; index < a.length && i <= sum / a[index]; i++) {
+
+			// System.out.println(sum +" "+i*a[index]);
+			if (sum == i * a[index])
+				count += 1;
+			else if (sum > i * a[index])
+				count = count + (getCoinChange((sum - i * a[index]), index + 1));
+			else if (sum < i * a[index])
+				count = count + (getCoinChange((sum), index + 1));
+
+		}
+
+		mem.put(index + "-" + sum, count);
+		return count;
+	}
+
 }
---
+++ HackerRank/src/algorithms/dynamic/archive/CoinChangeProblem.java
@@ -0,0 +1,78 @@
+package algorithms.dynamic.archive;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
+
+
+public class CoinChangeProblem {
+
+	//static int a[]={10,2,1};
+	static int a[];
+	static int count;
+	public static void main(String[] args) {
+		
+		/*HashMap<Integer,Integer> h= new HashMap<Integer,Integer>();
+		h.put(1, 1);
+		h.put(2, 5);
+		
+		HashMap<Integer,Integer> i= new HashMap<Integer,Integer>();
+		i.put(3, 1);
+		i.put(4, 5);
+		i.putAll(h);
+		
+			System.out.println(i);
+		*/
+		
+		
+		Scanner s= new Scanner(System.in);
+		int n=s.nextInt();
+		/*4 3
+		1 2 3 */
+		int m=s.nextInt();
+		
+		a=new int[m];
+		for(int i=0;i<m;i++){
+			a[i]=s.nextInt();
+		}
+		
+		getCoinChange(n,a, new HashMap<Integer,Integer>());
+		
+		System.out.println(count);
+	}
+	
+	public static void getCoinChange(int sum, int[] sub, HashMap<Integer,Integer> x){
+			//System.out.println("INPUT ARRAY : "+Arrays.toString(sub));
+		
+			if(sub.length==0){
+	
+			return;
+			}
+			int a=sub[0];		
+			for(int j=0;j<=sum/sub[0];j++){
+				if(j*a==sum){
+					x.put(a, j);
+					count++;
+					System.out.println("FOUND "+x);
+					x.remove(a);
+				}
+				else if(j*a>sum){
+					break;
+				}else{
+				//	op=op+""+a+"x"+j+" + ";
+					//System.out.println(sum+" "+x);
+					x.put(a, j);
+
+					getCoinChange(sum-a*j, Arrays.copyOfRange(sub, 1,sub.length ),x);
+				}
+				
+			}
+		
+
+		
+		
+	}
+	
+	
+	
+}
