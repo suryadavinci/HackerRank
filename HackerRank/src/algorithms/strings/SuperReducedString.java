a3b2d0c1b89a2e9d95c4466bdf02fd2c4c5ed8d6
gnanasuriyan.g <gnanasuriyan.g@D7240.prodapt.com>
2017-03-10 14:46:35 +0530

    super reduced string

---
+++ HackerRank/src/algorithms/strings/SuperReducedString.java
@@ -0,0 +1,27 @@
+package algorithms.strings;
+
+import java.util.Scanner;
+
+public class SuperReducedString {
+
+	public static void main(String[] args) {
+
+
+		String s= new Scanner(System.in).next();
+		//String s="abcccdaaa";
+		
+		int i=0;
+		while(i<s.length()-1){
+			if(s.charAt(i)==s.charAt(i+1))
+				{
+					s=s.substring(0,i)+s.substring(i+2);
+				}
+			else
+				i++;
+		}
+		
+		System.out.println(s);
+		
+	}
+
+}
