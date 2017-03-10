b6957e3168f841a2ad10d6889181612884101edb
gnanasuriyan.g <gnanasuriyan.g@D7240.prodapt.com>
2017-03-10 15:01:28 +0530

    Camel Case first try

---
+++ HackerRank/src/algorithms/strings/CamelCase.java
@@ -0,0 +1,22 @@
+package algorithms.strings;
+
+import java.util.Scanner;
+
+public class CamelCase {
+
+	public static void main(String[] args) {
+		String s= new Scanner(System.in).next();
+		
+		int count=1;
+		
+		
+		for(int i=1;i<s.length();i++){
+		//	if(s.charAt(i)<91)
+			if(s.charAt(i)>=65 && s.charAt(i)<91)
+				count++;
+		}
+		System.out.println(count);
+		
+		
+	}
+}
